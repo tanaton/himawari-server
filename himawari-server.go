35f42cbb14104945db0686aea519f6e364b07b95
tanaton <heiwaboke@gmail.com>
2018-11-18 12:47:30 +0900

    デフォルトのエンコード割り当てスレッド数を減らした

--- himawari-server.go
+++ himawari-server.go
@@ -38,7 +38,7 @@ const (
 	COMPLETED_DELETE_DURATION = time.Hour * 24 * 7
 
 	PRESET_NAME                 = "libx265-hq-ts.ffpreset"
-	ENCODE_THREADS              = 12
+	ENCODE_THREADS              = 2
 	THUMBNAIL_INTERVAL_DURATION = time.Second * 10
 )
 
