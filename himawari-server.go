ba0912cddae463d9fca5e4e8397811c04f959264
tanaton <heiwaboke@gmail.com>
2019-01-03 01:15:09 +0900

    1ワーカー複数エンコードが可能になるように修正

--- himawari-server.go
+++ himawari-server.go
@@ -37,7 +37,8 @@ const (
 	WORKER_DELETE_DURATION    = time.Hour * 24
 	COMPLETED_DELETE_DURATION = time.Hour * 24 * 7
 
-	PRESET_NAME                 = "libx265-hq-ts.ffpreset"
+	PRESET_PREFIX               = "libx265-hq-ts_"
+	PRESET_EXT                  = ".ffpreset"
 	ENCODE_THREADS              = 0
 	THUMBNAIL_INTERVAL_DURATION = time.Second * 10
 )
@@ -178,6 +179,7 @@ func (hh *himawariHandle) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 			// お仕事を得る
 			t := hh.taskToWorker(r)
 			if t != nil {
+				pname := PRESET_PREFIX + t.Id + PRESET_EXT
 				tt := struct {
 					Task
 					PresetName string
@@ -186,14 +188,14 @@ func (hh *himawariHandle) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 					Args       []string
 				}{
 					Task:       *t,
-					PresetName: PRESET_NAME,
+					PresetName: pname,
 					PresetData: PRESET_DATA,
 					Command:    "ffmpeg",
 					Args: []string{
 						"-y",
 						"-i", fmt.Sprintf("http://%s:%d/video/id/%s", serverIP, HTTP_PORT, t.Id),
 						"-threads", strconv.FormatInt(getHeaderDec(r, "X-Himawari-Threads", ENCODE_THREADS), 10),
-						"-fpre", PRESET_NAME,
+						"-fpre", pname,
 						"-vcodec", "libx265",
 						"-acodec", "aac", // libfdk_aac
 						"-ar", "48000",
